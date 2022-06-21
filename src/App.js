c97eac29104b5c36217471a50ce831d411f521eb
Aris Vayas <aris.vayas@gmail.com>
2022-06-21 15:10:51 -0400

    added default home route

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import React from "react";
 import Home from "./Home";
 import NewUserForm from "./NewUserForm";
 import ShoppingCart from "./ShoppingCart";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import NavBar from "./NavBar";
 function App() {
   return (
     <div className="App">
       <NavBar />
+
       <Routes>
+        <Route path="/" element={<Navigate replace to="/Home" />} />
         <Route path="/Home" element={<Home />}></Route>
         {/* <Route path="/about" element={<About />}></Route> */}
         <Route path="/Signup" element={<NewUserForm />}></Route>
